8e22fac6ed0db2103520df8123a178d13a91bdcf
AkshatGLA <tanishqsomani28@gmail.com>
2025-11-24 23:20:34 +0530

    JS Assignment

---
+++ js/HelloWorld.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+
+let a=10;
+var b=20;
+const c="akshat";
+let d=[1,2,3,4,5];
+
+let obj={
+    name:"akshat",
+    age:22,
+    city:"delhi"
+}
+
+console.log(a+" "+typeof(a));
+console.log(b+" "+typeof(b));
+console.log(c+" "+typeof(c));
+console.log(d+" "+typeof(d));
+console.log(obj+" "+typeof(obj));
